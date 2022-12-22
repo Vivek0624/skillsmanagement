36398a07cb0e8f3ea44f2649b069070efd7c8cca
Vivek Gundu <62993315+Vivek0624@users.noreply.github.com>
2022-12-22 20:34:54 +0530

    initial commit

---
+++ src/components/Modal.js
@@ -0,0 +1,194 @@
+import React from 'react';
+
+export default function Modal() {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <button
+        className='bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+        type='button'
+        onClick={() => setShowModal(true)}
+      >
+        Open large modal
+      </button>
+      {showModal ? (
+        <>
+          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+            <div className='relative w-auto my-6 mx-auto max-w-6xl'>
+              {/*content*/}
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                {/*header*/}
+                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
+                  <h3 className='text-3xl font-semibold'>Form</h3>
+                  {/* <button
+                    className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
+                      ×
+                    </span>
+                  </button> */}
+                </div>
+
+                {/*body*/}
+                <div className='relative p-6 flex-auto '>
+                  <form>
+                    <div class='relative z-0 mb-6 w-full group'>
+                      <input
+                        type='email'
+                        name='floating_email'
+                        id='floating_email'
+                        class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                        placeholder=' '
+                        required
+                      />
+                      <label
+                        for='floating_email'
+                        class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                      >
+                        Email address
+                      </label>
+                    </div>
+                    <div class='relative z-0 mb-6 w-full group'>
+                      <input
+                        type='password'
+                        name='floating_password'
+                        id='floating_password'
+                        class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                        placeholder=' '
+                        required
+                      />
+                      <label
+                        for='floating_password'
+                        class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                      >
+                        Password
+                      </label>
+                    </div>
+                    <div class='relative z-0 mb-6 w-full group'>
+                      <input
+                        type='password'
+                        name='repeat_password'
+                        id='floating_repeat_password'
+                        class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                        placeholder=' '
+                        required
+                      />
+                      <label
+                        for='floating_repeat_password'
+                        class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                      >
+                        Confirm password
+                      </label>
+                    </div>
+                    <div class='grid md:grid-cols-2 md:gap-6'>
+                      <div class='relative z-0 mb-6 w-full group'>
+                        <input
+                          type='text'
+                          name='floating_first_name'
+                          id='floating_first_name'
+                          class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                          placeholder=' '
+                          required
+                        />
+                        <label
+                          for='floating_first_name'
+                          class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                        >
+                          First name
+                        </label>
+                      </div>
+                      <div class='relative z-0 mb-6 w-full group'>
+                        <input
+                          type='text'
+                          name='floating_last_name'
+                          id='floating_last_name'
+                          class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                          placeholder=' '
+                          required
+                        />
+                        <label
+                          for='floating_last_name'
+                          class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                        >
+                          Last name
+                        </label>
+                      </div>
+                    </div>
+                    <div class='grid md:grid-cols-2 md:gap-6'>
+                      <div class='relative z-0 mb-6 w-full group'>
+                        <input
+                          type='tel'
+                          pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+                          name='floating_phone'
+                          id='floating_phone'
+                          class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                          placeholder=' '
+                          required
+                        />
+                        <label
+                          for='floating_phone'
+                          class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                        >
+                          Phone number (123-456-7890)
+                        </label>
+                      </div>
+                      <div class='relative z-0 mb-6 w-full group'>
+                        <input
+                          type='text'
+                          name='floating_company'
+                          id='floating_company'
+                          class='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+                          placeholder=' '
+                          required
+                        />
+                        <label
+                          for='floating_company'
+                          class='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+                        >
+                          Company (Ex. Google)
+                        </label>
+                      </div>
+                    </div>
+                    <button
+                      className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                      type='button'
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      type='submit'
+                      class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                      //   onClick={() => setShowModal(false)}
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+                {/*footer*/}
+                {/* <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+                  <button
+                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                    type='button'
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className='bg-emerald-500 text-blue active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                    type='button'
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div> */}
+              </div>
+            </div>
+          </div>
+          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+        </>
+      ) : null}
+    </>
+  );
+}
