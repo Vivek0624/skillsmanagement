c9ab5204c1ee90af9b79473e95d0e00b4c00e364
shri : ) <ciuser@ci.phoenix.xyz>
2023-01-03 17:33:38 +0530

    all Done V.1

--- skill_database_app/frontend/src/App.js
+++ skill_database_app/frontend/src/App.js
@@ -1,39 +1,47 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ListUserComponent from './components/ListUserComponent';
 import HeaderComponent from './components/HeaderComponent';
-// import FooterComponent from './components/FooterComponent';
+import FooterComponent from './components/FooterComponent';
 import CreateUserComponent from './components/CreateUserComponent';
 import LandingPage from './components/landing';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
-
-// import Navbar from './components/navbar'
+import NotFound from './components/NotFound';
+import ResponsiveAppBar from './components/reg';
 import ViewUserComponent from './components/ViewUserComponent';
+import getall from './components/GetAll';
+
 function App() {
   return (
     <div>
-      <Router>
-        <HeaderComponent />
+    
+        <Router>
 
-        <div className='  '>
-          {/*<sidebar></sidebar> */}
-          {/*<Navbar/>*/}
-          <Switch>
-            {/*<Route path = "/" exact component = {ListUserComponent}></Route>*/}
-            <Route path='/' exact component={LandingPage}></Route>
-            <Route path='/login' exact component={LoginPage} />
-            <Route path='/register' exact component={RegisterPage} />
-            <Route path='/users' component={ListUserComponent}></Route>
-            <Route path='/add-user/:id' component={CreateUserComponent}></Route>
-            <Route path='/view-user/:id' component={ViewUserComponent}></Route>
-            {/* <Route path = "/update-user/:id" component = {UpdateUserComponent}></Route> */}
-          </Switch>
-        </div>
-        {/* <FooterComponent /> */}
-      </Router>
+              <ResponsiveAppBar />
+              
+                <div className=" ">
+                {/*<sidebar></sidebar> */}
+                {/*<Navbar/>*/}
+                
+                    <Switch> 
+                          {/*<Route path = "/" exact component = {ListUserComponent}></Route>*/}
+                          <Route path = "/" exact component = {LandingPage}></Route>
+                          <Route path="/login" exact component={LoginPage} />
+                          <Route path="/register" exact component={RegisterPage} />
+                          <Route path = "/users" component = {ListUserComponent}></Route>
+                          <Route path = "/add-user/:id" component = {CreateUserComponent}></Route>
+                          <Route path = "/view-user/:id" component = {ViewUserComponent}></Route>
+                          <Route path = "/getall" component = {getall}></Route>
+                          {/* <Route path = "/update-user/:id" component = {UpdateUserComponent}></Route> */}
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
+    
   );
 }
 
--- skill_database_app/frontend/src/components/FooterComponent.jsx
+++ skill_database_app/frontend/src/components/FooterComponent.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 class FooterComponent extends Component {
     constructor(props) {
@@ -14,6 +17,7 @@ class FooterComponent extends Component {
             <div>
                 <footer className = "footer">
                     <span className="text-muted">All Rights Reserved 2022 @Soul-cathers</span>
+                    <br></br><GitHubIcon /> <FacebookIcon /> <TwitterIcon />          
                 </footer>
             </div>
         )
